09bfb9531d5c8a3232f3914c474060f3dd332dbc
Luiz Serão <luiz.serao@hotmail.com>
2024-03-18 08:09:43 -0300

    Home Categories section

---
+++ frontend/src/components/Home/Categories/Categories.tsx
@@ -0,0 +1,21 @@
+import Category from './Category'
+import Dining from '../../../imgs/home/categories/dining.png'
+import Living from '../../../imgs/home/categories/living.png'
+import Bedroom from '../../../imgs/home/categories/bedroom.png'
+import styles from './Categories.module.css'
+
+const Categories = () => {
+  return (
+    <section className={`${styles.categories} container`}>
+      <h2 className={styles.title}>Browse The Range</h2>
+      <p className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <ul className={styles.listCategories}>
+        <Category src={Dining} alt='Dining' title='Dining' pathCategory='dining' />
+        <Category src={Living} alt='Living' title='Living' pathCategory='living' />
+        <Category src={Bedroom} alt='Bedroom' title='Bedroom' pathCategory='bedroom' />
+      </ul>
+    </section>
+  )
+}
+
+export default Categories
