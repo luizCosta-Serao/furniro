4dd3a80e91b408da764146ae6245272a1501b94c
Luiz Serão <luiz.serao@hotmail.com>
2024-04-10 08:48:51 -0300

    checkout and stripe payment

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const routes = express.Router()
+require('dotenv').config()
+const stripe = require('stripe')(process.env.STRIPE_API_SECRET)
 
 const ProductsController = require('./controllers/ProductsController')
 const ProductController = require('./controllers/ProductController')
@@ -8,5 +10,38 @@ const ProductController = require('./controllers/ProductController')
 routes.get('/products', ProductsController.read)
 routes.post('/products', ProductsController.create)
 routes.get('/products/:id', ProductController.read)
+
+// Rotas Stripe
+routes.post('/create-checkout-session', async (req, res) => {
+  const { cartItems } = req.body
+  const line_items = cartItems.map((item) => {
+    return {
+      price_data: {
+        currency: 'usd',
+        product_data: {
+          name: item.product.name,
+          images: [item.product.image],
+          description: item.product.description,
+          metadata: {
+            id: item.product._id
+          }
+        },
+        unit_amount: item.product.price * 100,
+      },
+      quantity: item.quantity,
+    }
+  })
+
+  const session = await stripe.checkout.sessions.create({
+    line_items,
+    mode: 'payment',
+    success_url: `${process.env.CLIENT_URL}/checkout-success`,
+    cancel_url: `${process.env.CLIENT_URL}/checkout`,
+  });
+
+  res.send({
+    url: session.url
+  });
+});
 
 module.exports = routes
---
+++ frontend/src/components/Checkout/HeroCheckout.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Hero from '../Global/Hero/Hero'
+
+const HeroCheckout = () => {
+  return (
+    <Hero title='Checkout' firstPath='Home' secondPath='Checkout' />
+  )
+}
+
+export default HeroCheckout
