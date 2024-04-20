fb283674769d312fff92898ef4e2b616c32b6539
Luiz Serão <luiz.serao@hotmail.com>
2024-04-20 06:52:30 -0300

    try 4 mongodb fix

--- backend/src/config/dbConfig.js
+++ backend/src/config/dbConfig.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
-const dbConfig = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eouo0vp.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0`
+const dbConfig = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.eouo0vp.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0`
 
 const connection = mongoose.connect(dbConfig)
 
